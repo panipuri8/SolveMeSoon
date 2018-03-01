899141ebe5669cef07d3d702196364544f71dbb4
Akshay V <akshayred@gmail.com>
2018-03-01 14:18:34 +0530

    First commit, added problem validation using requests feature

---
+++ SolveMeSoon/dbhandler.py
@@ -0,0 +1,114 @@
+import pymysql
+import requests
+
+SERVER_NAME = "localhost"
+DB_USERNAME = "root"
+DB_PASSWORD = "password"
+DB_NAME = "mysql"
+
+USERS_TABLE = "cp_todo_list_users"
+PROBLEMS_TABLE = "cp_todo_list_problems"
+
+SOME_ERROR = 1001
+
+ADDED_NEW_USER = 5001
+USERNAME_ALREADY_EXISTS = 5002
+
+VALID_LOGIN = 6001
+INVALID_USERNAME = 6002
+WRONG_PASSWORD = 6003
+
+EMPTY_TODO_LIST = 7001
+SUCCESSFUL_TODO_LIST = 7002
+
+PROBLEM_ADDED = 8001
+PROBLEM_ALREADY_EXISTS = 8002
+INVALID_PROBLEM  = 8003
+NOT_CONNECTED_TO_INTERNET = 8004
+
+def add_new_user(username,password,email):
+	try:
+		db = pymysql.connect(SERVER_NAME , DB_USERNAME , DB_PASSWORD ,DB_NAME)
+		cursor = db.cursor()
+		sql = "select * from cp_todo_list_users where username=%s"
+		cursor.execute(sql, (username))
+		data = cursor.fetchall()
+		if(len(data)>0):
+			return USERNAME_ALREADY_EXISTS
+		sql = "insert into cp_todo_list_users(username,password,email_id) values(%s,%s,%s)"
+		cursor.execute(sql, (username,password,email))
+		db.commit()
+		return ADDED_NEW_USER
+	except Exception as e:
+		print("EXCEPTION IN DBHANDLER",e.__class__.__name__)
+		print(e)
+		db.rollback()
+		return SOME_ERROR
+
+def login(username,password):
+	try:
+		db = pymysql.connect(SERVER_NAME , DB_USERNAME , DB_PASSWORD ,DB_NAME)
+		cursor = db.cursor()
+		sql = "select * from cp_todo_list_users where username=%s"
+		cursor.execute(sql, (username))
+		data = cursor.fetchall()
+		if(len(data)<=0):
+			return INVALID_USERNAME
+		if(len(data)>1):
+			return SOME_ERROR
+		actual_password = data[0][1] #contains the password
+		if(actual_password == password):
+			return VALID_LOGIN
+		else:
+			return WRONG_PASSWORD
+	except Exception as e:
+		print("EXCEPTION IN DBHANDLER",e.__class__.__name__)
+		print(e)
+		return SOME_ERROR
+
+def get_problems(username):
+	try:
+		db = pymysql.connect(SERVER_NAME , DB_USERNAME , DB_PASSWORD ,DB_NAME)
+		cursor = db.cursor()
+		sql = "select problemid from cp_todo_list_problems as one inner join cp_todo_list_users as two on one.username =%s and one.username = two.username"
+		cursor.execute(sql, (username))
+		data = cursor.fetchall()
+		if(len(data)<=0):
+			return EMPTY_TODO_LIST
+		myDict = [{"problemid":row[0]} for row in data]
+		return myDict
+	except Exception as e:
+		print("EXCEPTION IN DBHANDLER",e.__class__.__name__)
+		print(e)
+		return SOME_ERROR
+
+def add_problem(username, problemid):
+	try:
+		r = requests.head("https://www.codechef.com/problems/" + problemid)
+		# print(r.status_code)
+		if(r.status_code != 200):
+			print("Invalid problem here")
+			return INVALID_PROBLEM
+		db = pymysql.connect(SERVER_NAME , DB_USERNAME , DB_PASSWORD ,DB_NAME)
+		cursor = db.cursor()
+		sql = "select * from cp_todo_list_problems where username=%s and problemid=%s"
+		cursor.execute(sql,(username,problemid))
+		data = cursor.fetchall()
+		if(len(data)>0):
+			return PROBLEM_ALREADY_EXISTS
+		sql = "insert into cp_todo_list_problems(problemid,username) values(%s,%s)"
+		cursor.execute(sql,(problemid,username))
+		db.commit()
+		return PROBLEM_ADDED
+	except requests.ConnectionError:
+		return NOT_CONNECTED_TO_INTERNET
+	except Exception as e:
+		print("EXCEPTION IN DBHANDLER",e.__class__.__name__)
+		print(e)
+		db.rollback()
+		return SOME_ERROR
+
+
+if(__name__ == '__main__'):
+	x = add_problem('panipuri8' , 'SOOTHU')
+	print(x)
---
+++ SolveMeSoon/server.py
@@ -0,0 +1,75 @@
+from flask import Flask
+from flask import jsonify
+from flask import abort
+from flask import make_response
+from flask import request
+import dbhandler
+
+app = Flask(__name__,static_url_path="")
+
+@app.route("/signup",methods=['POST'])
+def signup():
+	try:
+		data = request.get_json(force=True)
+		result = dbhandler.add_new_user(data['uname'],data['pass'],data['mail'])
+		if(result == dbhandler.USERNAME_ALREADY_EXISTS):
+			return jsonify({"Error":"Username already exists"})
+		elif(result == dbhandler.ADDED_NEW_USER):
+			return jsonify({"Success":"Added"})
+		else:
+			raise Exception
+	except Exception as e:
+		return jsonify({"Error":"Something went wrong"})
+
+@app.route("/login",methods=['POST'])
+def login():
+	try:
+		data = request.get_json(force=True)
+		result = dbhandler.login(data['uname'],data['pass'])
+		if(result == dbhandler.VALID_LOGIN):
+			return jsonify({"Success":"Added"})
+		elif(result == dbhandler.INVALID_USERNAME):
+			return jsonify({"Error":"Username does not exist"})
+		elif(result == dbhandler.WRONG_PASSWORD):
+			return jsonify({"Error":"Wrong Password"})
+	except Exception as e:
+		print(e)
+		return jsonify({"Error":"Something went wrong"})
+
+@app.route("/problems" , methods=['POST'])
+def get_problems():
+	try:
+		data = request.get_json(force=True)
+		result = dbhandler.get_problems(data['uname'])
+		if(result == dbhandler.EMPTY_TODO_LIST):
+			return jsonify({"Empty":"Empty ToDo List"})
+		else:
+			return jsonify(result)
+	except Exception as e:
+		print(e)
+		return jsonify({"Error":"Something went wrong"})
+
+@app.route("/addproblem" , methods=['POST'])
+def add_problem():
+	try:
+		data = request.get_json(force=True)
+		result = dbhandler.add_problem(data['uname'],data['problemid'])
+		if(result == dbhandler.PROBLEM_ALREADY_EXISTS):
+			print("SERVER.py" , "ALREADY EXISTS")
+			return jsonify({"Problem":"Problem already exists in your todo list!"})
+		elif(result == dbhandler.INVALID_PROBLEM):
+			print("SERVER.py" , "DOES NOT EXIST")
+			return jsonify({"Problem":"Invalid Problem Code"})
+		elif(result == dbhandler.NOT_CONNECTED_TO_INTERNET):
+			print("SERVER.py" , "NO INTERNET")
+			return jsonify({"Problem":"Connect to the internet while adding problems!"})
+		elif(result == dbhandler.PROBLEM_ADDED):
+			print("SERVER.py" , "PROBLEM ADDED BECAUSE IT WAS NEW !")
+			return jsonify({"Success":"Added"})
+	except Exception as e:
+		print(e)
+		return jsonify({"Error":"Something went wrong"})
+
+
+if(__name__ == '__main__'):
+	app.run(debug = True)
